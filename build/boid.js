1d6231c721762b6fb0fd325f600085f24bd2c66e
Erencan Pelin <erencan.pelin@gmail.com>
2023-03-31 16:42:26 +1100

    Add cohesion rule

--- build/boid.js
+++ build/boid.js
@@ -1,11 +1,11 @@
 import * as THREE from 'three';
 
 var BoidSettings = {
-    cohesion: 0.5,
+    cohesion: 0.2,
     separation: 0.5,
     alignment: 0.2,
-    awareness: 10,
-    moveSpeed: 4,
+    awareness: 30,
+    moveSpeed: 2,
     worldSize: 200,
 };
 
@@ -22,10 +22,13 @@ class Boid {
         this.acceleration = new THREE.Vector3();
     }
 
-    update () {
+    update() {
         this.position.add(this.velocity);
         this.velocity.add(this.acceleration);
+        this.velocity.clampLength(-BoidSettings.moveSpeed, BoidSettings.moveSpeed);
         this.boidMesh.position.set(this.position.x, this.position.y, this.position.z);
+
+        this.acceleration.set(0, 0, 0);
     }
 
     edgeAvoid() {
@@ -47,22 +50,24 @@ class Boid {
 
     flock(boids) {
         let alignment = this.align(boids);
-        this.acceleration.set(alignment.x, alignment.y, alignment.z);
+        let cohesion = this.cohesion(boids);
+        this.acceleration.add(alignment);
+        this.acceleration.add(cohesion);
     }
 
     align(boids) {
-        var avg = new THREE.Vector3(0,0,0);
+        var avg = new THREE.Vector3(0, 0, 0);
         let total = 0;
         //loop through every nearby boid and set your own direction to the average of everyone elses
         for (let other of boids) {
             let distance = this.position.distanceTo(other.position);
-            
+
             if (other != this && distance < BoidSettings.awareness) {
                 avg.add(other.velocity);
                 total++;
             }
         }
-        
+
         if (total > 0) { //if there are actually boids near us
             avg.divideScalar(total);
             avg.setLength(BoidSettings.moveSpeed);
@@ -72,7 +77,30 @@ class Boid {
         return avg;
     }
 
-    createBoid (scene) {
+    cohesion(boids) {
+        var avg = new THREE.Vector3(0, 0, 0);
+        let total = 0;
+        //loop through every nearby boid and set your own direction to the average of everyone elses
+        for (let other of boids) {
+            let distance = this.position.distanceTo(other.position);
+
+            if (other != this && distance < BoidSettings.awareness) {
+                avg.add(other.position);
+                total++;
+            }
+        }
+
+        if (total > 0) { //if there are actually boids near us
+            avg.divideScalar(total);
+            avg.sub(this.position);
+            avg.setLength(BoidSettings.moveSpeed);
+            avg.sub(this.velocity);
+            avg.clampLength(-BoidSettings.cohesion, BoidSettings.cohesion);
+        }
+        return avg;
+    }
+
+    createBoid(scene) {
         const boidGeometry = new THREE.SphereGeometry();
         const boidMat = new THREE.MeshBasicMaterial();
         boidMat.color = new THREE.Color(1, 1, 1);
