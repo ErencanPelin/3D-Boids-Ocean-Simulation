fb3d1b70d721c4eb26480a446fde896992b8c0fe
DoMess <michael.lam14@outlook.com>
2023-06-01 13:44:36 +1000

    Added a line trail

--- vfx/particle-system.js
+++ vfx/particle-system.js
@@ -1,5 +1,15 @@
+import * as THREE from 'three';
 
+const trailGeometry = new THREE.BufferGeometry();
+// const trailGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+const trailMaterial = new THREE.LineBasicMaterial({ 
+    color: 0x1313ff, 
+    linewidth: 1,
+    linecap: 'square',
+    linejoin: 'round', 
+});
+// const trailMaterial = new THREE.MeshBasicMaterial({ color: 0x1313ff });
+const trailPoints = []; // Array to hold trail points
 
-export function CreateSystem() {
-    
-}
+export { trailGeometry, trailMaterial, trailPoints };
+  
