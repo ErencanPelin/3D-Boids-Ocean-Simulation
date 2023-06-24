a32814c3168f33fae6d1145364e2ee835ed24b32
Unizzu <nathankylegalang@gmail.com>
2023-06-24 14:12:20 +1000

    seaweed!

---
+++ build/loadmisc.js
@@ -0,0 +1,57 @@
+import * as THREE from 'three';
+import { GLTFLoader } from './loaders/GLTFLoader.js';
+
+export function loadseaweed(scene, pos) {
+    var mixer;
+    seaweedload(scene, pos, mixer);
+}
+
+async function seaweedload(scene, pos, mixer)
+{
+    var coralloader = new GLTFLoader();
+    var loadPromise = coralloader.loadAsync('../models/sea_weed/scene.gltf');
+        await loadPromise.then(function (gltf) {
+            
+            gltf.scene.position.set(pos.x, pos.y-2, pos.z);
+            gltf.scene.rotation.y += randDir();
+            gltf.scene.scale.set(10,10,10);
+
+            mixer = new THREE.AnimationMixer( gltf.scene );
+
+            scene.add(gltf.scene);
+            //console.log(gltf.animations);
+            
+            var Clock = new THREE.Clock();
+
+            const clip = THREE.AnimationClip.findByName( gltf.animations, 'Sway' );
+            const action = mixer.clipAction( clip );
+            action.play();
+
+            var update = function()
+            {
+                var deltaSeconds = Clock.getDelta();
+                if(mixer != null)
+                {
+                    //console.log("playing...");
+                    mixer.update( deltaSeconds );
+                }
+                requestAnimationFrame(update);
+            }
+            requestAnimationFrame(update);
+
+        }).catch();
+}
+
+function randModifier ()
+{
+    var randNum = Math.floor(Math.random()*5) + 1;
+    randNum *= Math.round(Math.random()) ? 1 : -1; 
+    return randNum;
+}
+
+function randDir ()
+{
+    var randNum = Math.floor(Math.random()*180) + 1;
+    randNum *= Math.round(Math.random()) ? 1 : -1; 
+    return randNum;
+}
